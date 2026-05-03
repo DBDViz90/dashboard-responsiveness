05effca974f66afd514db279b4904ee4c01ed0c8
DBDViz90 <david.bekri@ik.me>
2026-05-03 19:53:54 +0200

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/dashboard-owid-responsiveness/',
+  base: '/dashboard-responsiveness/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
